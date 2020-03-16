4fc2cf7dca3c817983a1a8b4b5674af840670e6f
Gunnerdud <61637274+Gunnerdud@users.noreply.github.com>
2020-03-16 15:31:49 -0400

    Update function.js

--- javascript/function.js
+++ javascript/function.js
@@ -77,27 +77,10 @@ $(document).ready(function() {
             var application_id = application.id;
 
             if (application_id) {
-                var query = '
-                    UPDATE
-                        project_application
-                    SET
-                        Preference_Order = '${preference}',
-                        Date = '${date}'
-                    WHERE
-                        Application_ID = '${application_id}'
-                '; postResults(query);
+                var query = 'UPDATE project_application SET Preference_Order = '${preference}', Date = '${date}' WHERE Application_ID = '${application_id}''; postResults(query);
 
             } else {
-                var query = '
-                    INSERT INTO
-                        project_application
-                    VALUES (
-                        '',
-                        '${student_id}',
-                        '${project_id}',
-                        '${preference}',
-                        '${date}'
-                )'; postResults(query);
+                var query = 'INSERT INTO project_application VALUES ('','${student_id}','${project_id}','${preference}','${date}')'; postResults(query);
             };
 
         }); refreshPage();
@@ -130,16 +113,7 @@ $(document).ready(function() {
 // ========================================
 
 function renderPreferenceSelect() {
-    var query = '
-        SELECT
-            student.Student_ID,
-            CONCAT_WS(" ",
-                student.FirstName,
-                student.LastName
-            ) AS "Student"
-        FROM
-            student
-    ';
+    var query = 'SELECT student.Student_ID,CONCAT_WS(" ",student.FirstName,student.LastName) AS "Student" FROM student';
 
     Promise.all([
         getResults(query),
